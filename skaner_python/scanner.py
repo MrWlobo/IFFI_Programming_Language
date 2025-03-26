8db2b1b4f06444e6e3a83737643667c6c638b024
Mateusz Battek <mbattek@student.agh.edu.pl>
2025-03-26 15:49:09 +0100

    Reading from file, coloring in HTML file

--- skaner_python/scanner.py
+++ skaner_python/scanner.py
@@ -3,7 +3,7 @@ init()
 
 class Token:
 
-    token_colors = {"IDENTIFIER": Fore.RED, "INTEGER": Fore.BLUE, "BRACKET": Fore.GREEN, "OPERATION": Fore.YELLOW}
+    token_colors = {"IDENTIFIER": Fore.RED, "INTEGER": Fore.BLUE, "BRACKET": Fore.GREEN, "OPERATION": Fore.YELLOW, "WHITESPACE": Fore.WHITE}
 
     def __init__(self, token_id, token_value):
         self.token_id = token_id
@@ -21,11 +21,10 @@ class Scanner:
     def scan_integer(self, expression):
         integer = ""
         while (self.index < len(expression) and
-               (expression[self.index].isdigit()
-                or expression[self.index].isspace())):
+               (expression[self.index] in token_ids["NUMBER"])):
 
             # Ignore whitespaces between parts of the same integer
-            if not expression[self.index].isspace():
+            if not expression[self.index] in token_ids["WHITESPACE"]:
                 integer += expression[self.index]
             self.index += 1
 
@@ -34,32 +33,35 @@ class Scanner:
     def scan_identifier(self, expression):
         identifier = ""
         while (self.index < len(expression) and
-               (expression[self.index].isalpha()
-                or expression[self.index].isdigit()
-                or expression[self.index] == "_"
-                or expression[self.index].isspace())):
+               (expression[self.index] in token_ids["LETTER"]
+                or expression[self.index] in token_ids["NUMBER"])):
 
-            # Ignore whitespaces between components of an idenifier
-            if not expression[self.index].isspace():
+            # Ignore whitespaces between components of an identifier
+            if not expression[self.index] in token_ids["WHITESPACE"]:
                 identifier += expression[self.index]
             self.index += 1
 
         return Token("IDENTIFIER", identifier)
 
     def scan_whitespace(self, expression):
-        while expression[self.index].isspace():
+        whitespace = ""
+        while self.index < len(expression) and expression[self.index] in token_ids["WHITESPACE"]:
+            whitespace += expression[self.index]
             self.index += 1
+        return Token("WHITESPACE", whitespace)
 
     def scanner(self, expression):
+        if self.index >= len(expression):
+            return None
 
         # Ignore whitespaces after operators and brackets
-        if expression[self.index].isspace():
-            self.scan_whitespace(expression)
+        if expression[self.index] in token_ids["WHITESPACE"]:
+            return self.scan_whitespace(expression)
 
-        if expression[self.index].isalpha() or expression[self.index] == "_":  # When letter or _ - identifier
+        elif expression[self.index] in token_ids["LETTER"]:  # When letter or _ - identifier
             return self.scan_identifier(expression)
 
-        elif expression[self.index].isdigit():  # When digit - integer
+        elif expression[self.index] in token_ids["NUMBER"]:  # When digit - integer
             return self.scan_integer(expression)
 
         elif expression[self.index] in self.token_ids["BRACKET"]:
@@ -74,11 +76,35 @@ class Scanner:
             raise Exception(
                 "Scanner error: Unknown character " + expression[self.index] + " in column " + str(self.index))
 
-expression = "ab    (   cd   + 21     5   )  as ds -  * 12 dsa782 (21 fh  32 j    )"
+file_path = 'test_file'
 
-token_ids = {"INTEGER": ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"], "BRACKET": ["(", ")"],
-             "OPERATION": ["+", "-", "*", "/"]}
+with open(file_path, 'r') as file:
+    file_content = file.read()
+
+expression = file_content
+print(expression)
+
+token_ids = {"LETTER": "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_",
+             "NUMBER": "0123456789",
+             "BRACKET": "()[]{}",
+             "OPERATION": "+-*/",
+             "WHITESPACE": " \n\t"}
+token_colors = {"IDENTIFIER": "red", "INTEGER": "blue", "BRACKET": "green", "OPERATION": "orange", "WHITESPACE": "black"}
+
 scanner = Scanner(token_ids)
 
+f = open("test_file.html", "w")
+
 while scanner.index < len(expression):
-    print(scanner.scanner(expression))
+    token = scanner.scanner(expression)
+    print(token)
+    if token is None:
+        break
+    if token.token_id == "WHITESPACE":
+        temp_whitespace = token.token_value.replace(" ", "&nbsp;")
+        temp_whitespace = temp_whitespace.replace("\n", "<br>")
+        temp_whitespace = temp_whitespace.replace("\t", "&#9;")
+        f.write(temp_whitespace)
+    else:
+        f.write('<span style="color:' + token_colors[token.token_id] + '">' + token.token_value + '</span>')
+f.close()
